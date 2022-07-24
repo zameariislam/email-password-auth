133d587c5c4e7bbcbfc4e5b9876500d7c0394ef2
zameariislam <zameariislam@gmail.com>
2022-07-24 07:47:58 +0200

    toggle between register and sign in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from 'react';
 import app from './firebase.init'
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,6 +10,10 @@ const auth = getAuth(app)
 const App = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [registered, setRegistered] = useState(false)
+  const [validated, setValidated] = useState(false);
+  const [error, setError] = useState('');
+ 
 
   const handleEmailBlur = (e) => {
     setEmail(e.target.value)
@@ -21,6 +25,19 @@ const App = () => {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+     event.stopPropagation();
+     return;
+    }
+    if(!/(?=.*[A-Z])/.test(password)){
+      setError('password should contain atleast one Upercase letter')
+      return;
+
+    }
+    setError('')
+
+    setValidated(true);
     
     createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
@@ -36,30 +53,38 @@ const App = () => {
     });
 
   }
-
-
- 
-     
+const handleRegisteredChange=(e)=>{
+  setRegistered(e.target.checked)
+  
+}
 
   return (
     <div className="w-50 mx-auto mt-2">
-      <h2 className="text-primary">Please Register</h2>
-      <Form onSubmit={handleFormSubmit}>
+      <h2 className="text-primary">{registered?'please logIn' : 'Please Register'}</h2>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control required onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+        <Form.Control.Feedback type="invalid">
+            Please provide a Email.
+          </Form.Control.Feedback>
         
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control required onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+        <Form.Control.Feedback type="invalid">
+            Please provide a valid Password.
+          </Form.Control.Feedback>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+        <Form.Check  onChange={handleRegisteredChange}  type="checkbox" label="already registerd ?" />
       </Form.Group>
+      <Form.Text>{error}</Form.Text>
+      
       <Button variant="primary" type="submit">
-        Submit
+    {registered? 'LogIn':'Register'}
       </Button>
     </Form>
     </div>
