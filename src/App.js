ae6e8bc80f842634887f763d8271b1e8820c7f13
zameariislam <zameariislam@gmail.com>
2022-07-24 06:10:03 +0200

    react-bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from 'react';
+import app from './firebase.init'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import React from 'react';
+const auth = getAuth(app)
 
 const App = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value)
+
+  }
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(email, password)
+
+  }
   return (
     <div>
-      <h1>I am APP</h1>
-      
+      <form onSubmit={handleFormSubmit} >
+        <input onBlur={handleEmailChange} type="email" /> <br />
+        <input onBlur={handlePasswordChange} type="password" /> <br />
+      <input type="submit" value="LogIn" />
+
+      </form>
+
     </div>
   );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,22 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAWUSJtI9o9iXway_J6UXupzE6NRPO_K-I",
-    authDomain: "email-password-app-906ac.firebaseapp.com",
-    projectId: "email-password-app-906ac",
-    storageBucket: "email-password-app-906ac.appspot.com",
-    messagingSenderId: "904006676764",
-    appId: "1:904006676764:web:056b8d2548636d1226cad9",
-    measurementId: "G-VE8JF2CQ5X"
+  apiKey: "AIzaSyCJozuidH7RPD4yRZRM-g5SfFduSWYZmCU",
+  authDomain: "email-password-auth-f2211.firebaseapp.com",
+  projectId: "email-password-auth-f2211",
+  storageBucket: "email-password-auth-f2211.appspot.com",
+  messagingSenderId: "79602762611",
+  appId: "1:79602762611:web:eb1d529de57e87f9b6fb06"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export default app
+export default app;
