98d92528427aa9ba377ffa1ba636435c53becb86
zameariislam <zameariislam@gmail.com>
2022-07-24 08:02:12 +0200

    emial-verification

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from 'react';
 import app from './firebase.init'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Form,Button } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 
 const auth = getAuth(app)
@@ -13,7 +13,7 @@ const App = () => {
   const [registered, setRegistered] = useState(false)
   const [validated, setValidated] = useState(false);
   const [error, setError] = useState('');
- 
+
 
   const handleEmailBlur = (e) => {
     setEmail(e.target.value)
@@ -27,10 +27,10 @@ const App = () => {
     event.preventDefault();
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
-     event.stopPropagation();
-     return;
+      event.stopPropagation();
+      return;
     }
-    if(!/(?=.*[A-Z])/.test(password)){
+    if (!/(?=.*[A-Z])/.test(password)) {
       setError('password should contain atleast one Upercase letter')
       return;
 
@@ -38,60 +38,80 @@ const App = () => {
     setError('')
 
     setValidated(true);
+
+    // Existing User 
+
+    if (registered) {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          console.log(user)
+          // ...
+        })
+        .catch((error) => {
+         console.log(error)
+        });
+
+    }
+
+    // new User 
+else{
+  createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user)
+        // ...
+      })
+      .catch((error) => {
+        console.log(error)
+
+        // ..
+      });
+}
     
-    createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      // Signed in 
-      const user = userCredential.user;
-      console.log(user)
-      // ...
-    })
-    .catch((error) => {
-      console.log(error)
-      
-      // ..
-    });
 
   }
-const handleRegisteredChange=(e)=>{
-  setRegistered(e.target.checked)
-  
-}
+  const handleRegisteredChange = (e) => {
+    setRegistered(e.target.checked)
+
+  }
 
   return (
     <div className="w-50 mx-auto mt-2">
-      <h2 className="text-primary">{registered?'please logIn' : 'Please Register'}</h2>
+      <h2 className="text-primary">{registered ? 'please logIn' : 'Please Register'}</h2>
       <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control required onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
-        <Form.Control.Feedback type="invalid">
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control required onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+          <Form.Control.Feedback type="invalid">
             Please provide a Email.
           </Form.Control.Feedback>
-        
-      </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control required onBlur={handlePasswordBlur} type="password" placeholder="Password" />
-        <Form.Control.Feedback type="invalid">
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control required onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+          <Form.Control.Feedback type="invalid">
             Please provide a valid Password.
           </Form.Control.Feedback>
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check  onChange={handleRegisteredChange}  type="checkbox" label="already registerd ?" />
-      </Form.Group>
-      <Form.Text>{error}</Form.Text>
-      
-      <Button variant="primary" type="submit">
-    {registered? 'LogIn':'Register'}
-      </Button>
-    </Form>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check onChange={handleRegisteredChange} type="checkbox" label="already registerd ?" />
+        </Form.Group>
+        <Form.Text>{error}</Form.Text>
+
+        <Button variant="primary" type="submit">
+          {registered ? 'LogIn' : 'Register'}
+        </Button>
+      </Form>
     </div>
-   
+
   );
 }
-  
+
 
 
 
